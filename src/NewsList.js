85090a655ce4a774a775433f4337d75199572883
EC2 Default User <ec2-user@ip-172-31-25-121.eu-west-2.compute.internal>
2020-05-18 13:31:23 +0000

    Second Commit

---
+++ src/NewsList.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class NewsList extends Component{
+    /*constructor(props){
+        super(props);
+        this.state = {
+            news: []
+        };
+    } */
+    
+    render(){
+        return(
+            <div>
+                <h1> NewsList! </h1>
+            </div>
+            );
+    }
+}
+
+export default NewsList;
