07b2873b849e0873e25ffbe2fe2b93d4698e1b9c
EC2 Default User <ec2-user@ip-172-31-25-121.eu-west-2.compute.internal>
2020-06-15 22:49:42 +0000

    error handler seems right

--- src/Oops.js
+++ src/Oops.js
@@ -6,14 +6,12 @@ class Oops extends Component{
 
         return(
         <div    className="oops">
-                
-            <h2 > Oops! Something went wrong. </h2>
-            <p2 > The most likely cause therefore is that the daily amount of free news request on NewsAPI have been exceeded (500 per day). </p2>
-            <p2 > In a way that is great news in it self, since it means the site is frequently used!! </p2>
-            <p2 > If this keeps happinnig I update the hosting package to allow for more traffic. </p2>
+            <div className="oops-item" > <h2 > Oops! Something went wrong. </h2> </div>
+            <div className="oops-item" > <p2 > The most likely cause therefore is that the daily amount of free news request on NewsAPI have been exceeded (500 per day). </p2> </div>
+            <div className="oops-item" > <p2 > In a way that is great news in it self, since it means the site is frequently used!! </p2> </div>
+            <div className="oops-item" > <p2 > If this keeps happinnig I update the hosting package to allow for more traffic. </p2></div>
             
-            <h2 > Have great day and please come back tomorrow! </h2>
-            
+            <div className="oops-item" > <h2 > Have great day and please come back tomorrow! </h2> </div>
         </div>
             );
     }
