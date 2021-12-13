9bf9362015afbe86a5679df59fb07e8cec6fec55
jschoemig <julian.schoemig@hotmail.de>
2021-12-13 23:30:23 +0000

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import wordlist from './WordList';
 import sources from './Sources';
 
-const url           = 'https://newsapi.org/v2/everything?qInTitle=';         // everthing and then filtered
+const url           = 'http://newsapi.org/v2/everything?qInTitle=';         // everthing and then filtered
 const qInTitle      =  wordlist;                                    // only q for search in title and body
 const s             =  sources;
 const key           = '&sortBy=relevancy&apiKey=1894a0623e654d328392e2cc7ff458a4';      // sortBy popularity - relevancy - publishedAt (default)
