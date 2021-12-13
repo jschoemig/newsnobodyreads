6ceaadbd6dfeeff79922d0350bfd64eb39029196
jschoemig <julian.schoemig@hotmail.de>
2021-12-13 23:47:01 +0000

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -7,7 +7,6 @@ const s             =  sources;
 const key           = '&sortBy=relevancy&apiKey=1894a0623e654d328392e2cc7ff458a4';      // sortBy popularity - relevancy - publishedAt (default)
 const proxyUrl      = 'https://cors-anywhere.herokuapp.com/';       // this proxy url serves as middleware - hence, my api request connects with heroku and they make the API call for me
 
-Access-Control-Allow-Origin: *
         
  
 // https://crossorigin.me/ or https://cors-anywhere.herokuapp.com/
