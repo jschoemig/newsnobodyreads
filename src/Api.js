c5ccc0792377da3fae53137367bb599cc07597ec
jschoemig <julian.schoemig@hotmail.de>
2021-12-14 12:50:42 +0000

    no cors

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import wordlist from './WordList';
 import sources from './Sources';
 
-const url           = 'newsapi.org/v2/everything?qInTitle=';         // everthing and then filtered
+const url           = 'https://newsapi.org/v2/everything?qInTitle=';         // everthing and then filtered
 const qInTitle      =  wordlist;                                    // only q for search in title and body
 const s             =  sources;
 const key           = '&sortBy=relevancy&apiKey=1894a0623e654d328392e2cc7ff458a4';      // sortBy popularity - relevancy - publishedAt (default)
@@ -20,12 +20,12 @@ function handleErrors(resp) {
 
   export async function getNews(val,search){
       if(search){
-        return fetch(proxyUrl + url + val + s + key)            // because it is an async function 
+        return fetch( url + val + s + key)            // because it is an async function 
                 .then(handleErrors)
                 .catch( err =>  console.log(err) );
 
       } else {
-        return fetch(proxyUrl + url + qInTitle + s + key)            // because it is an async function 
+        return fetch( url + qInTitle + s + key)            // because it is an async function 
                 .then(handleErrors)
                 .catch( err =>  console.log(err) );
       }
